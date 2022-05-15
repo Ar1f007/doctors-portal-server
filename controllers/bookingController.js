cfa43e9bf27a4f9979c6493b7116b41ed221b52b
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-16 02:26:59 +0600

    create booking api is modified so that it checks before creating booking time slot is available or not on the server

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,9 +9,26 @@ exports.createBooking = async (req, res) => {
     patientName: booking.patientName,
   };
 
-  const alreadyBooked = await bookingCollection.findOne(query);
-  if (alreadyBooked) {
-    return res.send({ success: false, booking: alreadyBooked });
+  const alreadyBookedByTheSameUser = await bookingCollection.findOne(query);
+
+  if (alreadyBookedByTheSameUser) {
+    return res.send({
+      success: false,
+      message: `You have already booked an appointment for it on ${alreadyBookedByTheSameUser?.date} at ${alreadyBookedByTheSameUser?.slot}`,
+    });
+  }
+
+  const slotAlreadyTaken = await bookingCollection.findOne({
+    treatment: booking.treatment,
+    slot: booking.slot,
+    date: booking.date,
+  });
+
+  if (slotAlreadyTaken) {
+    return res.send({
+      success: false,
+      message: 'Sorry, the time slot you picked is already taken. Please try another slot.',
+    });
   }
   const response = await bookingCollection.insertOne(booking);
   return res.send({ success: true, response });
